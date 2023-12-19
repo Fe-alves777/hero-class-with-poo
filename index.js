8368d051ee48de97d4bbf4aa363355a564f155a6
Fe-alves777 <rm98122@fiap.com.br>
2023-12-19 20:31:13 -0300

    first commit of this project

---
+++ index.js
@@ -0,0 +1,64 @@
+
+class heroi {
+    constructor (nome, idade, tipo) {
+
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    atacar() {
+        let ataque = " ";
+        switch (this.tipo) {
+
+            case "mago": 
+                ataque = "magia";
+                break
+
+            case "guerreiro":
+                ataque = "espada";
+                break
+            
+            case "monge":
+                ataque ="artes marciais";
+                break
+            
+            case "ninja":
+                ataque ="shuriken";
+                break
+            
+            default:
+                ataque ="comum";
+
+
+        } 
+
+        //alert(`O ${this.tipo} atacou usando ${ataque}`);
+        console.log(`A ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+
+    atacarSe() {
+        let ataque = " ";
+        if (this.tipo === "monge") {
+            ataque = "artes marciais";
+        } else if (this.tipo === "ninja") {
+            ataque = "shuriken";
+        } else if (this.tipo === "guerreiro") {
+            ataque = "espada";
+        } else if (this.tipo === "mago") {
+            ataque = "magia";
+        } else {
+            ataque = "comum";
+        }
+
+        //alert(`O ${this.tipo} atacou usando ${ataque}`);
+        console.log(`O ${this.tipo} ${this.nome} atacou usando ${ataque}`);
+    }
+}
+
+let personagemFeminino = new heroi ("Maria", 9, "monge");
+
+let personagemMasculino = new heroi ("Felipe", 21, "mago");
+
+personagemFeminino.atacar();
+personagemMasculino.atacarSe();
